d0e58b00e392ed46542fd9a72db6e192221fa63d
Rui Duan <rui.duan@loblaw.ca>
2021-08-12 23:10:45 -0400

    Pass gigasecond tests

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,10 @@
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Duration, Utc};
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    unimplemented!("What time is a gigasecond later than {}", start);
+    let one_nanosec = Duration::seconds(1_000_000_000);
+    match start.checked_add_signed(one_nanosec) {
+        Some(dt) => dt,
+        None => start,
+    }
 }
