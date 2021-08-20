51858c1e71aa980dec40bf3f46d9379dc19e82eb
Rui Duan <rui.duan@loblaw.ca>
2021-08-20 04:09:23 -0400

    Proverb: functional style

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,19 +1,15 @@
+use std::iter;
+
 pub fn build_proverb(list: &[&str]) -> String {
-    let mut result: String = String::new();
-    let length: usize = list.len();
-
-    if length == 0 {
-        return result;
+    if list.is_empty() {
+        return String::new();
     }
 
-    let mut i: usize = 0;
-    while i < length - 1 {
-        let a = list[i];
-        let b = list[i + 1];
-        result.push_str(&format!("For want of a {} the {} was lost.\n", a, b));
-        i += 1;
-    }
-    result.push_str(&format!("And all for the want of a {}.", list[0]));
-
-    result
+    list.windows(2)
+        .map(|pair| format!("For want of a {} the {} was lost.\n", pair[0], pair[1]))
+        .chain(iter::once(format!(
+            "And all for the want of a {}.",
+            list[0]
+        )))
+        .collect()
 }
