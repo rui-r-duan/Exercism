f07cc4824705f7239ccca3d79d4585c33e3ab42e
Rui Duan <rui.duan@loblaw.ca>
2021-10-25 22:33:47 -0400

    space age v3 with declarative macro

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -1,6 +1,3 @@
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
-
 #[derive(Debug)]
 pub struct Duration {
     years: f64,
@@ -22,43 +19,21 @@ pub trait Planet {
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
-
-impl Planet for Mercury {
-    const EARTH_YEAR_RATIO: f64 = 0.2408467;
+#[macro_export]
+macro_rules! defplanet {
+    ($planet_name:ident, $earth_year_ratio:expr) => {
+        pub struct $planet_name;
+        impl Planet for $planet_name {
+            const EARTH_YEAR_RATIO: f64 = $earth_year_ratio;
+        }
+    };
 }
 
-impl Planet for Venus {
-    const EARTH_YEAR_RATIO: f64 = 0.61519726;
-}
-
-impl Planet for Earth {
-    const EARTH_YEAR_RATIO: f64 = 1.0;
-}
-
-impl Planet for Mars {
-    const EARTH_YEAR_RATIO: f64 = 1.8808158;
-}
-
-impl Planet for Jupiter {
-    const EARTH_YEAR_RATIO: f64 = 11.862615;
-}
-
-impl Planet for Saturn {
-    const EARTH_YEAR_RATIO: f64 = 29.447498;
-}
-
-impl Planet for Uranus {
-    const EARTH_YEAR_RATIO: f64 = 84.016846;
-}
-
-impl Planet for Neptune {
-    const EARTH_YEAR_RATIO: f64 = 164.79132;
-}
+defplanet!(Mercury, 0.2408467);
+defplanet!(Venus, 0.61519726);
+defplanet!(Earth, 1.0);
+defplanet!(Mars, 1.8808158);
+defplanet!(Jupiter, 11.862615);
+defplanet!(Saturn, 29.447498);
+defplanet!(Uranus, 84.016846);
+defplanet!(Neptune, 164.79132);
