3633849243b949445540e853582900a808e0cf23
Rui Duan <rui.duan@loblaw.ca>
2021-11-15 22:16:27 -0500

    RPN Calculator v6

--- rust/rpn-calculator/src/lib.rs
+++ rust/rpn-calculator/src/lib.rs
@@ -1,5 +1,3 @@
-use std::ops::{Add, Div, Mul, Sub};
-
 #[derive(Debug)]
 pub enum CalculatorInput {
     Add,
@@ -9,28 +7,39 @@ pub enum CalculatorInput {
     Value(i32),
 }
 
-fn binary_op(stack: &mut Vec<i32>, f: impl Fn(i32, i32) -> i32) -> Option<i32> {
-    stack.pop().and_then(|y| stack.pop().map(|x| f(x, y)))
-}
-
 pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
-    inputs
-        .iter()
-        .try_fold(vec![], |mut stack, input| {
-            match input {
-                CalculatorInput::Add => binary_op(&mut stack, i32::add),
-                CalculatorInput::Subtract => binary_op(&mut stack, i32::sub),
-                CalculatorInput::Multiply => binary_op(&mut stack, i32::mul),
-                CalculatorInput::Divide => binary_op(&mut stack, i32::div),
-                CalculatorInput::Value(value) => Some(*value),
+    let mut stack: Vec<i32> = vec![];
+    for input in inputs {
+        match input {
+            CalculatorInput::Add => {
+                let b = stack.pop()?;
+                let a = stack.pop()?;
+                stack.push(a + b);
             }
-            .map(|x| {
-                stack.push(x);
-                stack
-            })
-        })
-        .and_then(|stack| match stack.as_slice() {
-            [x] => Some(*x),
-            _ => None,
-        })
+            CalculatorInput::Subtract => {
+                let b = stack.pop()?;
+                let a = stack.pop()?;
+                stack.push(a - b);
+            }
+            CalculatorInput::Multiply => {
+                let b = stack.pop()?;
+                let a = stack.pop()?;
+                stack.push(a * b);
+            }
+            CalculatorInput::Divide => {
+                let b = stack.pop()?;
+                let a = stack.pop()?;
+                stack.push(a / b);
+            }
+            CalculatorInput::Value(val) => {
+                stack.push(*val);
+            }
+        }
+    }
+
+    if stack.len() > 1 {
+        return None;
+    }
+
+    stack.pop()
 }
