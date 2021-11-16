24e903d305922e9e7e8304b77a5d3359d11d6b40
Rui Duan <rui.duan@loblaw.ca>
2021-11-15 22:41:37 -0500

    RPN Calculator v7

--- rust/rpn-calculator/src/lib.rs
+++ rust/rpn-calculator/src/lib.rs
@@ -1,3 +1,5 @@
+use std::ops::{Add, Div, Mul, Sub};
+
 #[derive(Debug)]
 pub enum CalculatorInput {
     Add,
@@ -7,39 +9,59 @@ pub enum CalculatorInput {
     Value(i32),
 }
 
+/// Returns (first_operand, second_operand)
+fn extract_operands(stack: &mut Vec<i32>) -> (Option<i32>, Option<i32>) {
+    let b = match stack.pop() {
+        Some(x) => Some(x),
+        None => None,
+    };
+    let a = match stack.pop() {
+        Some(x) => Some(x),
+        None => None,
+    };
+    (a, b)
+}
+
+fn calc(op_a: Option<i32>, op_b: Option<i32>, stack: &mut Vec<i32>, f: impl Fn(i32, i32) -> i32) {
+    match op_a {
+        Some(a) => match op_b {
+            Some(b) => {
+                let c = f(a, b);
+                stack.push(c);
+            }
+            None => (),
+        },
+        None => (),
+    }
+}
+
 pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
-    let mut stack: Vec<i32> = vec![];
-    for input in inputs {
-        match input {
+    let mut stack: Vec<i32> = Vec::new();
+    for token in inputs {
+        match token {
             CalculatorInput::Add => {
-                let b = stack.pop()?;
-                let a = stack.pop()?;
-                stack.push(a + b);
+                let (op_a, op_b) = extract_operands(&mut stack);
+                calc(op_a, op_b, &mut stack, i32::add);
             }
             CalculatorInput::Subtract => {
-                let b = stack.pop()?;
-                let a = stack.pop()?;
-                stack.push(a - b);
+                let (op_a, op_b) = extract_operands(&mut stack);
+                calc(op_a, op_b, &mut stack, i32::sub);
             }
             CalculatorInput::Multiply => {
-                let b = stack.pop()?;
-                let a = stack.pop()?;
-                stack.push(a * b);
+                let (op_a, op_b) = extract_operands(&mut stack);
+                calc(op_a, op_b, &mut stack, i32::mul);
             }
             CalculatorInput::Divide => {
-                let b = stack.pop()?;
-                let a = stack.pop()?;
-                stack.push(a / b);
+                let (op_a, op_b) = extract_operands(&mut stack);
+                calc(op_a, op_b, &mut stack, i32::div);
             }
-            CalculatorInput::Value(val) => {
-                stack.push(*val);
+            CalculatorInput::Value(v) => {
+                stack.push(*v);
             }
         }
     }
-
-    if stack.len() > 1 {
-        return None;
+    match stack.as_slice() {
+        [x] => Some(*x),
+        _ => None,
     }
-
-    stack.pop()
 }
