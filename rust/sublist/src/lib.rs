1a92b06f6bb0dbcd51f589d6b871199632cef1e0
Rui Duan <rui.duan@loblaw.ca>
2021-11-22 17:57:42 -0500

    sublist v2

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -7,50 +7,35 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(first: &[T], second: &[T]) -> Comparison {
-    let len_a = first.len();
-    let len_b = second.len();
-    if len_a == len_b {
-        if sublist_recur(first, second) {
+    let (m, n) = (first.len(), second.len());
+    if m == 0 && n == 0 {
+        Comparison::Equal
+    } else if m == 0 {
+        // n > 0
+        Comparison::Sublist
+    } else if n == 0 {
+        // m > 0
+        Comparison::Superlist
+    } else if m == n {
+        // 0 < m == n
+        if first == second {
             Comparison::Equal
         } else {
             Comparison::Unequal
         }
-    } else if len_a < len_b {
-        if len_a == 0 {
+    } else if m < n {
+        // 0 < m < n
+        if second.windows(m).any(|x| x == first) {
             Comparison::Sublist
         } else {
-            for (i, e) in second.iter().enumerate() {
-                if e == &first[0] {
-                    if sublist_recur(first, &second[i..]) {
-                        return Comparison::Sublist;
-                    }
-                }
-            }
-
             Comparison::Unequal
         }
     } else {
-        match sublist(second, first) {
-            Comparison::Equal => Comparison::Equal,
-            Comparison::Sublist => Comparison::Superlist,
-            Comparison::Superlist => Comparison::Sublist,
-            Comparison::Unequal => Comparison::Unequal,
+        // 0 < n < m
+        if first.windows(n).any(|x| x == second) {
+            Comparison::Superlist
+        } else {
+            Comparison::Unequal
         }
-    }
-}
-
-fn sublist_recur<T: PartialEq>(a: &[T], b: &[T]) -> bool {
-    let len_a = a.len();
-    let len_b = b.len();
-    if len_a > len_b {
-        return false;
-    }
-
-    if len_a == 0 {
-        true
-    } else if a[0] == b[0] {
-        sublist_recur(&a[1..], &b[1..])
-    } else {
-        false
     }
 }
