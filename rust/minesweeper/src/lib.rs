fb74627742715584f9ecc61bcf10e05ac6a078c2
Rui Duan <rui.duan@loblaw.ca>
2021-11-29 18:42:53 -0500

    minesweep v2

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -1,66 +1,41 @@
-use std::slice::Iter;
+const NEIBOURHOOD_OFFSETS: [(i32, i32); 8] = [
+    (-1, 0),
+    (-1, 1),
+    (0, 1),
+    (1, 1),
+    (1, 0),
+    (1, -1),
+    (0, -1),
+    (-1, -1),
+];
 
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-    let m = minefield.len(); // rows
-    if m == 0 {
-        return Vec::new();
-    }
-
-    let n = minefield[0].len(); // columns
-    if n == 0 {
-        return vec!["".to_string()];
-    }
-
-    let mut board = build_board(minefield);
-    for i in 0..m {
-        for j in 0..n {
-            if board[i][j] == ' ' {
-                let v = get_value(&board, i as i16, j as i16);
-                if v > 0 {
-                    // assume that the number of mines < 10
-                    if let Some(x) = v.to_string().chars().next() {
-                        board[i][j] = x;
+    let rows = minefield.len() as i32;
+    (0..rows)
+        .map(|x| {
+            let cols = minefield[x as usize].len() as i32;
+            (0..cols)
+                .map(|y| {
+                    let cell = minefield[x as usize].as_bytes()[y as usize];
+                    match cell {
+                        b'*' => '*',
+                        _ => {
+                            let count = NEIBOURHOOD_OFFSETS
+                                .iter()
+                                .map(|&(dx, dy)| (x + dx, y + dy))
+                                .filter(|&(r, c)| (0 <= r && r < rows) && (0 <= c && c < cols))
+                                .filter(|&(x, y)| {
+                                    minefield[x as usize].as_bytes()[y as usize] == b'*'
+                                })
+                                .count();
+                            match count {
+                                0 => ' ',
+                                n => (n as u8 + '0' as u8) as char,
+                            }
+                        }
                     }
-                }
-            }
-        }
-    }
-
-    board.iter().map(|r| r.iter().collect()).collect()
-}
-
-fn build_board(minefield: &[&str]) -> Vec<Vec<char>> {
-    let mut board: Vec<Vec<char>> = Vec::new();
-    for &row in minefield {
-        board.push(row.chars().collect());
-    }
-    board
-}
-
-fn get_value(board: &Vec<Vec<char>>, r: i16, c: i16) -> u8 {
-    let rows = board.len() as i16;
-    assert!(rows > 0);
-    let cols = board[0].len() as i16;
-    assert!(cols > 0);
-    let scan = |pos: Iter<(i16, i16)>| {
-        pos.fold(0u8, |mut accm: u8, &(r, c)| {
-            if r >= 0 && r < rows && c >= 0 && c < cols {
-                let i = r as usize;
-                let j = c as usize;
-                accm += if board[i][j] == '*' { 1u8 } else { 0u8 };
-            }
-            accm
+                })
+                .collect()
         })
-    };
-    let positions = [
-        (r - 1, c),
-        (r - 1, c + 1),
-        (r, c + 1),
-        (r + 1, c + 1),
-        (r + 1, c),
-        (r + 1, c - 1),
-        (r, c - 1),
-        (r - 1, c - 1),
-    ];
-    scan(positions.iter())
+        .collect()
 }
