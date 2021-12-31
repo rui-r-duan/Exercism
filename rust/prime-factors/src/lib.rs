82a49a4dbf3588a0e0dd264e68c0eefe921df540
Rui Duan <rui.duan@loblaw.ca>
2021-12-30 23:09:25 -0500

    prime factors v2

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,47 +1,16 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    if n == 1 {
-        return vec![];
-    }
+    let mut i = n;
+    let mut factors = Vec::new();
+    let mut candidates = 2..;
 
-    let mut result = vec![];
-    let mut quotient = n;
-    for p in generate_primes(n) {
-        while quotient != 1 {
-            if quotient % p == 0 {
-                result.push(p);
-                quotient = quotient / p;
-            } else {
-                break;
-            }
-        }
-        if quotient == 1 {
-            break;
+    while i > 1 {
+        let x = candidates.next().unwrap();
+
+        while i % x == 0 {
+            i /= x;
+            factors.push(x);
         }
     }
 
-    result
-}
-
-fn generate_primes(n: u64) -> Vec<u64> {
-    assert!(n > 1);
-
-    let mut a = vec![true; (n + 1).try_into().unwrap()];
-
-    let sqn = (n as f64).sqrt().floor() as usize;
-    for i in 2..=sqn {
-        if a[i] {
-            let mut k = 0;
-            loop {
-                let j = i.pow(2) + k * i;
-                if j <= (n as usize) {
-                    a[j] = false;
-                    k += 1;
-                } else {
-                    break;
-                }
-            }
-        }
-    }
-
-    (2..=n).filter(|&i| a[i as usize] == true).collect()
+    factors
 }
