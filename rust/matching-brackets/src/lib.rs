a17b2be7286bbbb992e1ca9805f6aafa52d2f10f
Rui Duan <rui.duan@loblaw.ca>
2022-01-10 21:58:21 -0500

    matching brackets v2

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,21 +1,24 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
     let mut stack: Vec<char> = Vec::new();
     for c in string.chars() {
-        if c == '{' || c == '[' || c == '(' {
-            stack.push(c);
-        } else if c == '}' || c == ']' || c == ')' {
-            if stack.is_empty() {
-                return false;
-            } else {
-                let &p = stack.last().unwrap();
-                if p == '{' && c == '}' || p == '[' && c == ']' || p == '(' && c == ')' {
-                    stack.pop();
-                } else {
+        match c {
+            '(' | '{' | '[' => stack.push(c),
+            ']' => {
+                if stack.pop() != Some('[') {
                     return false;
                 }
             }
-        } else {
-            continue;
+            '}' => {
+                if stack.pop() != Some('{') {
+                    return false;
+                }
+            }
+            ')' => {
+                if stack.pop() != Some('(') {
+                    return false;
+                }
+            }
+            _ => (),
         }
     }
 
