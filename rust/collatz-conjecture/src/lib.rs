282dde297a770f643d6152727f8fff65c944dd07
Rui Duan <rui.duan@loblaw.ca>
2022-01-13 21:44:59 -0500

    Collatz Conjecture v3

--- rust/collatz-conjecture/src/lib.rs
+++ rust/collatz-conjecture/src/lib.rs
@@ -6,22 +6,7 @@ pub fn collatz(n: u64) -> Option<u64> {
             0 => return None,
             1 => return Some(count),
             x if (x % 2) == 0 => n /= 2,
-            _ => match n.checked_mul(3) {
-                Some(x) => {
-                    n = x;
-                    match n.checked_add(1) {
-                        Some(x) => {
-                            n = x;
-                        }
-                        None => {
-                            return None;
-                        }
-                    }
-                }
-                None => {
-                    return None;
-                }
-            },
+            _ => n = n.checked_mul(3)?.checked_add(1)?,
         }
         count += 1;
     }
