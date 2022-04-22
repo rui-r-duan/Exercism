16805b4d00e8951c13064df40b11844549e3ded4
Rui Duan <rui.duan@loblaw.ca>
2022-04-22 03:39:15 -0400

    Binary search v2 generic

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,32 +1,16 @@
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    let n = array.len();
-    if n == 0 {
-        return None;
-    }
+pub fn find<C: AsRef<[T]>, T: Ord>(array: C, key: T) -> Option<usize> {
+    let slice = array.as_ref();
     let mut lo = 0;
-    let mut hi = n - 1;
-    loop {
-        if lo == hi {
-            if array[lo] == key {
-                return Some(lo);
-            } else {
-                return None;
-            }
-        } else if lo < hi {
-            let mid = lo + (hi - lo) / 2;
-            if array[mid] == key {
-                return Some(mid);
-            } else if array[mid] < key {
-                lo = mid + 1;
-            } else {
-                let new_hi = mid as i32 - 1;
-                if new_hi < 0 {
-                    return None;
-                }
-                hi = new_hi as usize;
-            }
+    let mut hi = slice.len() as i32 - 1;
+    while lo <= hi {
+        let mid = lo + (hi - lo) / 2;
+        if key < slice[mid as usize] {
+            hi = mid - 1;
+        } else if key > slice[mid as usize] {
+            lo = mid + 1;
         } else {
-            return None;
+            return Some(mid as usize);
         }
     }
+    None
 }
