d534178015e241bdbf9288dae0237d7a0e2f3d0c
Rui Duan <rui.duan@loblaw.ca>
2022-04-30 10:52:22 -0400

    macros v1

---
+++ rust/macros/src/lib.rs
@@ -0,0 +1,10 @@
+#[macro_export]
+macro_rules! hashmap {
+    ( $( $($key:expr => $value:expr)+ $(,)? )* ) => {
+        {
+            let mut temp_map = ::std::collections::HashMap::new();
+            $($(temp_map.insert($key, $value);)+)*
+            temp_map
+        }
+    }
+}
