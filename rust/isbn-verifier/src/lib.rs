52b3b417b58dcd6fa456b7c36d38ef93562bb3a8
Rui Duan <rui.duan@loblaw.ca>
2022-06-25 19:26:21 -0400

    ISBN verifier v2

--- rust/isbn-verifier/src/lib.rs
+++ rust/isbn-verifier/src/lib.rs
@@ -1,38 +1,19 @@
 /// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    let mut digits = [0u8; 11];
-    let mut pos = 11;
+    let mut sum = 0;
+    let mut count = 0;
     for c in isbn.chars() {
-        if pos > 2 {
-            if c.is_ascii_digit() {
-                pos -= 1;
-                digits[pos] = (c as u8) - b'0';
-            } else if c != '-' {
-                return false;
-            }
-        } else if pos == 2 {
-            if c.is_ascii_digit() {
-                pos -= 1;
-                digits[pos] = (c as u8) - b'0';
-            } else if c == 'X' {
-                pos -= 1;
-                digits[pos] = 10;
-            } else if c == '-' {
-                continue;
-            } else {
-                return false;
-            }
-        } else if pos == 1 {
-            return false; // isbn string is too long
+        if c.is_ascii_digit() && count < 10 {
+            sum += (10 - count) * c.to_digit(10).unwrap();
+            count += 1;
+        } else if c == 'X' && count == 9 {
+            sum += 10;
+            count += 1;
+        } else if c == '-' {
+            continue;
+        } else {
+            return false;
         }
     }
-    if pos != 1 {
-        // isbn string is too short
-        return false;
-    }
-    let mut sum = 0u32;
-    for (i, &v) in digits.iter().enumerate() {
-        sum += (i as u32) * (v as u32);
-    }
-    sum % 11 == 0
+    count == 10 && sum % 11 == 0
 }
