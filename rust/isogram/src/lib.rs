ec5e07a4f6e262b463c1cd6230094478a1e4f1ae
Rui Duan <rui.duan@loblaw.ca>
2022-06-26 15:31:56 -0400

    Isogram v3 HashSet, non-ASCII, filter(), all()

--- rust/isogram/src/lib.rs
+++ rust/isogram/src/lib.rs
@@ -1,13 +1,10 @@
 use std::collections::HashSet;
+
 pub fn check(candidate: &str) -> bool {
     let mut chars: HashSet<char> = HashSet::new();
-    for c in candidate.chars() {
-        if c != '-' && c != ' ' {
-            let c = c.to_ascii_lowercase();
-            if !chars.insert(c) {
-                return false;
-            }
-        }
-    }
-    true
+    candidate
+        .to_lowercase()
+        .chars()
+        .filter(|&c| c.is_alphabetic())
+        .all(|c| chars.insert(c))
 }
