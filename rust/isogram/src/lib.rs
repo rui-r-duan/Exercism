c7b3c190e1291a9998c13129d7471ba1542a6678
Rui Duan <rui.duan@loblaw.ca>
2022-06-26 15:31:12 -0400

    Isogram v2 HashSet

--- rust/isogram/src/lib.rs
+++ rust/isogram/src/lib.rs
@@ -1,19 +1,10 @@
-use std::collections::hash_map::Entry;
-use std::collections::HashMap;
-
+use std::collections::HashSet;
 pub fn check(candidate: &str) -> bool {
-    let mut chars: HashMap<char, bool> = HashMap::new();
+    let mut chars: HashSet<char> = HashSet::new();
     for c in candidate.chars() {
         if c != '-' && c != ' ' {
             let c = c.to_ascii_lowercase();
-            // if chars.contains_key(&c) {
-            //     return false;
-            // } else {
-            //     chars.insert(c, true);
-            // }
-            if let Entry::Vacant(e) = chars.entry(c) {
-                e.insert(true);
-            } else {
+            if !chars.insert(c) {
                 return false;
             }
         }
