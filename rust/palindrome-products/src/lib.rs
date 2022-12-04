b046fc8f60e29b0e1ec3dcb398f89d850e20a5f6
Rui Duan <rui.duan@loblaw.ca>
2022-12-03 22:32:09 -0500

    Palindrome Products v2

--- rust/palindrome-products/src/lib.rs
+++ rust/palindrome-products/src/lib.rs
@@ -8,17 +8,20 @@ pub struct Palindrome(u64);
 impl Palindrome {
     /// Create a `Palindrome` only if `value` is in fact a palindrome when represented in base ten. Otherwise, `None`.
     pub fn new(value: u64) -> Option<Palindrome> {
-        let vs = value.to_string();
-        let s = vs.as_bytes();
-        let mut i = 0;
-        let n = s.len();
-        while i < n / 2 {
-            if s[i] != s[n - i - 1] {
-                return None;
-            }
-            i += 1;
+        if value % 10 == 0 {
+            return None;
         }
-        Some(Palindrome(value))
+        let mut reverse = 0;
+        let mut r = value;
+        while r > 0 {
+            reverse = reverse * 10 + r % 10;
+            r /= 10;
+        }
+        if value == reverse {
+            Some(Palindrome(value))
+        } else {
+            None
+        }
     }
 
     /// Get the value of this palindrome.
@@ -32,7 +35,13 @@ pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome
     let mut largest = min * min;
     let mut found = false;
     for i in min..=max {
+        if i % 10 == 0 {
+            continue;
+        }
         for j in min..=max {
+            if j % 10 == 0 {
+                continue;
+            }
             let v = i * j;
             if Palindrome::new(v).is_some() {
                 if !found {
