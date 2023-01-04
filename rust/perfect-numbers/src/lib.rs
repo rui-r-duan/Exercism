7d86b6e6253c67d07b84a55d774fb937ac267f7f
Rui Duan <rui.duan@loblaw.ca>
2023-01-04 17:55:20 -0500

    perfect numbers v1

---
+++ rust/perfect-numbers/src/lib.rs
@@ -0,0 +1,40 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum Classification {
+    Abundant,
+    Perfect,
+    Deficient,
+}
+
+pub fn classify(num: u64) -> Option<Classification> {
+    if num == 0 {
+        return None;
+    } else if num == 1 {
+        return Some(Classification::Deficient);
+    }
+
+    let s: u64 = factors_without_self(num).iter().sum();
+    if s == num {
+        Some(Classification::Perfect)
+    } else if s > num {
+        Some(Classification::Abundant)
+    } else {
+        Some(Classification::Deficient)
+    }
+}
+
+fn factors_without_self(num: u64) -> Vec<u64> {
+    let mut result = vec![1];
+    let n: u64 = (num as f64).sqrt() as u64;
+    for i in 2..=n {
+        let q = num / i;
+        let r = num % i;
+        if r == 0 {
+            result.push(i);
+            if q > i {
+                result.push(q);
+            }
+        }
+    }
+
+    result
+}
